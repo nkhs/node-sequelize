d48d91ce37103cdb4b77a49b3e71dc15dee655d7
unknown <nkhs@yandex.com>
2021-03-23 00:29:16 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ let router = express.Router();
 
 let api = require(__dirname + "/../api/routes");
 
+router.get("/", (req, res) =>{
+    res.send('Ok')
+});
 router.use("/api", api);
 
 module.exports = router;
