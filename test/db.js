6082ef4582327397f4e797f63e03e9410de8b22d
nkhs <a0af@yandex.com>
2021-08-18 19:44:31 +0200

    working

--- test/db.js
+++ test/db.js
@@ -1,16 +1,24 @@
 'use strick';
 
-
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
-const db = require("../models");
+const db = require('../models');
 
 describe('db', () => {
     it('insert', async () => {
-        let createPost = await db.user.create({
-            name: 'test'
+        // let createPost = await db.user.create({
+        //     name: 'test'
+        // });
+        // var user = JSON.parse(JSON.stringify(createPost))
+        // console.log(user, user.id)
+        let sql = 'select * from users';
+        let records = await db.sequelize.query(sql, {
+            // replacements: {
+            //     userId: userId,
+            //     gameId: gameId,
+            // },
+            type: Sequelize.QueryTypes.SELECT,
         });
-        var user = JSON.parse(JSON.stringify(createPost))
-        console.log(user, user.id)
-    })
-})
+        console.log(records);
+    });
+});
